7a05989e52447555548a89ee36b22d109d79a5b6
Matěj Račinský <racinsky.matej@seznam.cz>
2023-05-11 10:35:19 +0200

    added note about tuning

---
+++ prepare_input_list.py
@@ -0,0 +1,15 @@
+import os
+import os.path as osp
+import glob
+# images_path = 'animefest AI original'
+images_path = 'animefest_selected_bench'
+extension = 'png'
+images = glob.glob(f"{images_path}/**/*.{extension}") + glob.glob(f"{images_path}/*.{extension}")
+cur_dir = os.getcwd()
+whole_paths = [osp.join(cur_dir, i) for i in images]
+# output_file = 'image_whole_list.txt'
+output_file = 'bench_list.txt'
+with open(output_file, 'w', encoding='utf-8') as f:
+    for i in whole_paths:
+        f.write(f'{i}\n')
+print('done')
